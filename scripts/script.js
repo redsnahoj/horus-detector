462d75a6c51b0b968f52f355b1d3ef67d1fbcc46
Johan Rojas <97570183+redsnahoj@users.noreply.github.com>
2023-07-01 18:47:54 -0400

    delete old script

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,3 @@
-/*
-const allLinks = document.getElementsByTagName("a");
-
-const filterLinks = Array.from(allLinks).filter((allLinks) =>
-{
-  return allLinks.href.includes(".zip") && allLinks.href.includes("@");
-});
-
-filterLinks.forEach((allLinks) => {
-  console.log(allLinks.href);
-});
-
-filterLinks.forEach((allLinks) => {
-  const warning = document.createElement("span");
-  warning.innerHTML = `
-  <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16" style="fill:red;">
-    <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-  </svg>
-  `;
-  allLinks.parentNode.insertBefore(warning, allLinks.nextSibling);
-  allLinks.href = "javascript:void(0)";
-  allLinks.style.pointerEvents = "none";
-  allLinks.style.color = "red";
-});
-
-*/
-
-// New code
-
 // Icon exclamation triangle
 const warning = 
   `<span class="warning"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
